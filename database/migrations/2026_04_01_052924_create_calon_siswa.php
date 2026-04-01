ae4861db25b75a73f576df1034e04ae153433341
AmuraAshbell <zidanrasyidsusanto2008@gmail.com>
2026-04-01 12:55:28 +0700

    first commit

---
+++ database/migrations/2026_04_01_052924_create_calon_siswa.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('calon_siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nik', 20)->unique();
+            $table->string('nama', 100);
+            $table->string('tempat_lahir', 50)->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->enum('jenis_kelamin', ['L', 'P'])->nullable();
+            $table->text('alamat')->nullable();
+            $table->string('asal_sekolah', 100)->nullable();
+            $table->string('no_hp', 15)->nullable();
+            $table->string('email', 100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('calon_siswa');
+    }
+};
